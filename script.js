3dd6359b0c4f2bbebf1f2ac5640b526378031d6f
Antor-094 <mdantor556677889@gmail.com>
2023-06-26 01:17:54 +0600

    Banner section + some about us section added

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
     document.querySelector('body').classList.toggle('mobile-nav-active')
     this.classList.toggle('fa-xmark')
  })
+
+ // typing effect
+ let typed = new Typed('.auto-input',{
+    strings:['Font-End Developer','Freelancer','UI Designer','YouTuber'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop:true
+ })
