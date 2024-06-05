1392286cacc971a093a36c6504dab865e769e79c
Antor-094 <mdantor556677889@gmail.com>
2024-06-05 21:46:51 +0600

    Project added

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+
+
 // header toggle
+document.addEventListener('contextmenu', function(event) {
+   event.preventDefault();
+ });
  let MenuBtn = document.getElementById('MenuBtn')
 
  MenuBtn.addEventListener('click',function(e){
@@ -8,9 +13,60 @@
 
  // typing effect
  let typed = new Typed('.auto-input',{
-    strings:['Font-End Developer','Freelancer','UI Designer','YouTuber'],
+    strings:['MERN Stack Developer'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:2000,
     loop:true
  })
+
+//  Active link  state on scroll
+
+// get all links
+let navLinks = document.querySelectorAll('nav ul li a')
+//get all sections
+let sections = document.querySelectorAll('section')
+window.addEventListener('scroll',function(){
+   const scrollPos = this.window.scrollY + 20
+   sections.forEach(section=>{
+      if(scrollPos>section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)){
+         navLinks.forEach(link=>{
+            link.classList.remove('active');
+            if(section.getAttribute('id')===link.getAttribute('href').substring(1)){
+               link.classList.add('active')
+            }
+         })
+      }
+   })
+})
+
+
+    document.getElementById('myForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Check if all fields are filled
+        var name = document.getElementById('name').value;
+        var email = document.getElementById('email').value;
+        var subject = document.getElementById('subject').value;
+        var msg = document.getElementById('msg').value;
+
+        if (!name || !email || !subject || !msg) {
+            alert('All fields must be filled!');
+            return;
+        }
+
+        // Send the email
+        emailjs.send('service_5rvargm', 'template_dbna3ws', {
+            from_name: name,
+            from_email: email,
+            subject: subject,
+            message: msg
+        })
+        .then(function(response) {
+            console.log('SUCCESS!', response.status, response.text);
+            alert('Email sent successfully');
+        }, function(error) {
+            console.log('FAILED...', error);
+            alert('Error occurred');
+        });
+    });
